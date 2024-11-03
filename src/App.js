ac0c30c0681bd2299bf2576f5bf06d436e3d3b71
wasif22001 <wasif22001@gmail.com>
2024-11-03 23:24:53 +0500

    created Navbar and Home components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
+
+  const tite = "Welcome to this new Blog";
+
+  const likes = 50;
+
+  const link = 'http://www.google.com';
+
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>App Component</h1>
+
+        <Home></Home>
+
+
+        <h1>{tite}</h1>
+        <p>Liked {likes} times</p>
+        <p> {"Hello"}</p>
+        <p> {10}</p>
+        <p> {[1,2,3,4,5]}</p>
+        <p> {Math.random() * 10}</p>
+
+        <a href={link}>Google site</a>
+
       </div>
     </div>
   );
